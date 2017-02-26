0a5e937e9e0b2ebc715d3ee7190a584d9b615d22
edRibeiro <eder.sonz@hotmail.com>
2017-02-26 10:41:09 -0300

    Como acessar os dados do formulário e exibir em outra página.

--- analise_decisoria/main/forms.py
+++ analise_decisoria/main/forms.py
@@ -3,3 +3,5 @@ from django import forms
 class captura_entrada_form(forms.Form):
     qtdeCriterio    = forms.IntegerField()
     qtdeAlternativa = forms.IntegerField()
+class FocoPrincipal_Form(forms.Form):
+    focoPrincipal = forms.CharField(label='Foco Principal', max_length=100)
--- analise_decisoria/main/views.py
+++ analise_decisoria/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import captura_entrada_form
+from .forms import captura_entrada_form, FocoPrincipal_Form
 
 # Create your views here.
 def menu_principal(request):
@@ -16,6 +16,13 @@ def sobre(request):
 
 def ahp_insert_valores(request):
     return render(request, 'ahp/ahp_insert_valores.html', {})
+
+def ahp_resultado(request):
+    resultado=request.POST['focoPrincipal'] ## é assim que recupera os dados de uma variavel.
+    return render(request, 'ahp/ahp_resultado.html', {'resultado': resultado})## Enviando o resultado para a próxima página.
+
+def ahp_foco_principal(request):
+    return render(request, 'ahp/ahp_foco_principal.html', {'form': FocoPrincipal_Form()})
 
 def qtdeCriterioAlternativa (request):
     if request.method == 'POST':
