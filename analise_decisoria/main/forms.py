94ceb0db7504b13fb9410af54ab8088241084157
edRibeiro <eder.sonz@hotmail.com>
2017-02-27 16:02:29 -0300

    Quantidade minima de criterios definida.

--- analise_decisoria/main/forms.py
+++ analise_decisoria/main/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from django.db import models
 
 class captura_entrada_form(forms.Form):
-    qtdeCriterio    = forms.IntegerField(min_value=0, max_value=99)
-    qtdeAlternativa = forms.IntegerField(min_value=0, max_value=99)
+    qtdeCriterio    = forms.IntegerField(min_value=2, max_value=99)
+    qtdeAlternativa = forms.IntegerField(min_value=2, max_value=99)
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=50)
@@ -19,6 +19,6 @@ class Alternativa_AHP_Form(forms.Form):
     alternativa = forms.CharField(label='Alternativa', max_length=100)
 
 class captura_entrada_AHP_Form(forms.Form):
-    qtdeCriterio    = forms.IntegerField(min_value=0, max_value=99)
-    qtdeAlternativa = forms.IntegerField(min_value=0, max_value=99)
+    qtdeCriterio    = forms.IntegerField(min_value=2, max_value=10)
+    qtdeAlternativa = forms.IntegerField(min_value=2, max_value=10)
     focoPrincipal = forms.CharField(max_length=100)
