5aba19f77b67c5b31e4e334365149349bfd1d572
Otho Garcia <othogar@gmail.com>
2017-02-27 16:15:35 -0300

    Elctre1 Matriz de Concordancia

--- analise_decisoria/main/views.py
+++ analise_decisoria/main/views.py
@@ -58,4 +58,49 @@ def csv_reader(request):
     """
     file = request.FILES['file']
     data = [row for row in csv.reader(file.read().splitlines())]
-    return render(request,'main/dados.html', {'dados': data})
+    alternativa = []
+    criterios = []
+    pesos = []
+    sinal = []
+    tabela= []
+    for i in range(len(data)-2):
+        if i != 0 :
+            alternativa.append(data[i][0])
+    for i in range(1,len(data[0])):
+        pesos.append(data[len(data)-2][i])
+        criterios.append(data[0][i])
+        sinal.append(data[len(data)-1][i])
+    for i in range(1,len(data)-2):
+        linha=[]
+        if i != 0 :
+            for j in range(1,len(data[0])):
+                teste = len(data[0])
+                linha.append(data[i][j])
+            tabela.append(linha)
+    teste = len(tabela[0])
+    mCon = matrizConcordanciaI(alternativa, tabela, len(tabela), len(tabela[0]), pesos)
+    return render(request,'main/dados.html', {'dados': alternativa, 'nLinhas': len(tabela[0])-1})
+
+
+def matrizConcordanciaI(cidades, tabela, nLinhas, nColunas, vetorPesos):
+
+	somaPesos = 0
+	mConcordancia = []
+
+	for x in range(len(vetorPesos)):
+		somaPesos += int(vetorPesos[x])
+
+
+	for i in range(nLinhas):
+		linha = []
+		for j in range(len(tabela[i])):
+
+			somatorioW = 0
+			for y in range(nColunas):
+				if tabela[i][y] >= tabela[j][y]:
+					somatorioW += int(vetorPesos[y])
+			result = 1.0/somaPesos * somatorioW
+			linha.append(round(result, 2))
+
+		mConcordancia.append(linha)
+	return mConcordancia
