5280f61740b6fdfdccffa84ee8985e7ddd4f5bef
Otho Garcia <othogar@gmail.com>
2017-02-27 12:52:36 -0300

    Merge

--- analise_decisoria/main/forms.py
+++ analise_decisoria/main/forms.py
@@ -3,12 +3,10 @@ from django import forms
 class captura_entrada_form(forms.Form):
     qtdeCriterio    = forms.IntegerField()
     qtdeAlternativa = forms.IntegerField()
-<<<<<<< HEAD
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=50)
     file = forms.FileField()
-=======
+
 class FocoPrincipal_Form(forms.Form):
     focoPrincipal = forms.CharField(label='Foco Principal', max_length=100)
->>>>>>> e1de9d24a032e4cb08b1439df9710aadd1fe730e
